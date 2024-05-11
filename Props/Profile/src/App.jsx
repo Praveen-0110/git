d4387a85384dded802a0d89fcec24a753f25d380
Praveen-0110 <saipraveen51186@gmail.com>
2024-05-11 21:09:55 +0530

    Practies

---
+++ Props/Profile/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Profile from './profile'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+   <img src="Picture/Photo -1.jpeg" alt="Picture" /> 
+    </>
+  )
+}
+
+export default App
